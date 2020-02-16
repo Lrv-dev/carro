39899895a237da8d7735a1182cda57154139d5a7
JackBowln <diasv3861@gmail.com>
2020-02-16 13:21:41 -0300

    Update Carro.py

--- Carro.py
+++ Carro.py
@@ -2,23 +2,26 @@ nome = input("Qual é o seu nome? \n")
 
 pergunta = (input("{}, você sabe a distância?\n ".lower().format(nome)))
 if pergunta.startswith('s'):
+
     distancia = float(input("qual é a distância?\n"))
 
     aproveitamento = float(input('{}, quantos quilometros seu carro faz por litro? \n'.format(nome)))
 
     precogas = int(input('{}, qual é o preço da gasolina atualmente?\n'.format(nome)))
 
-    consumo = distancia/aproveitamento
+    consumo = distancia / aproveitamento
 
     print("Foram gastos ", consumo, " litros de combustível.")
+
     custo = consumo * precogas
+
     print("E você vai gastar", custo, "reais")
 
 elif pergunta.startswith('n'):
 
-    import datetime
+    tempohoras = float(input("{}, digite o tempo gasto na viagem (Apenas quantas horas): \n".format(nome)))
 
-    tempo = float(input("{}, digite o tempo gasto na viagem (em minutos): \n".format(nome)))
+    tempominutos = float(input("{}, e quantos minutos? \n".format(nome)))
 
     velocidade = float(input('{}, digite agora a velocidade média durante a viagem:\n'.format(nome)))
 
@@ -26,14 +29,17 @@ elif pergunta.startswith('n'):
 
     precogas = float(input('{}, qual é o preço da gasolina atualmente?\n'.format(nome)))
 
-    consumo = (velocidade*(tempo/60))/aproveitamento
+    consumo = (velocidade * (tempohoras + (tempominutos / 60))) / aproveitamento
 
     print("Foram gastos ", consumo, " litros de combustível.")
 
     custo = consumo * precogas
 
-    distancia = velocidade * tempo
+    distancia = velocidade * (tempohoras + (tempominutos / 60))
 
-    print("E você vai gastar", custo,"reais")
+    print("E você vai gastar", custo, "reais")
 
     print("A distância percorrida foi de ", distancia, "quilometros.")
+
+else:
+    print ("responda sim ou não")
